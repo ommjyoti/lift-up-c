8659c49d59ba9d27430a8b8023c50b8d48515612
ommjyoti <78557950+ommjyoti@users.noreply.github.com>
2021-02-17 21:24:48 +0530

    Update Week 1 Q 1.cxx

--- Week 1 Q 1.cxx
+++ Week 1 Q 1.cxx
@@ -5,27 +5,14 @@ int main()
     char a;
     printf("Enter any two numbers:");
     scanf("%d %d",&x,&y);
-    printf("Enter your operation:");
-    scanf(" %c",&a);
-    switch(a)
-{
-    case '+':
-        printf("sum of two numbers:%d\n",x+y);
-        break;
-    case '-':
-        printf("substraction of two numbers:%d\n",x-y);
-        break;
-    case '*':
-        printf("multiplication of two numbers:%d\n",x*y);
-        break;
-    case '/':
+    
+    printf("sum of two numbers:%d\n",x+y);
+    printf("substraction of two numbers:%d\n",x-y);
+    printf("multiplication of two numbers:%d\n",x*y);
+    if(y==0){printf("Error : o in denominator\n");}
+    else{
         printf("division of two numbers:%d\n",x/y);
-        break;
-    case '%':
-        printf("remainder of two numbers:%d\n",x%y);
-        break;
-    default:
-        printf("please try again\n");
-}
+    }
+    printf("remainder of two numbers:%d\n",x%y);
 return 0;
 }
